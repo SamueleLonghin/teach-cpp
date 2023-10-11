7377546bfe2983091e14fae09d286b7cd47ec662
Samuele <longhin.samuele@outlook.com>
2023-10-11 09:29:38 +0200

    Aggiunto file su assegnamento

---
+++ introduzione/assign.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 6;
+    cout << a << endl;
+
+    a += 1;
+    cout << a << endl;
+
+    a += 1;
+    cout << a << endl;
+
+    a++;
+    cout << a << endl;
+
+    ++a;
+    cout << a << endl;
+
+    cout << a++ <<endl;
+
+    cout << ++a <<endl;
+    return 0;
+}
